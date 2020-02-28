7bb2a3578097c57fd0d1c2a9c8a3be4d65d226bd
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-02-28 12:35:13 -0800

    Wrote GET route for POSTS

--- models/Posts.js
+++ models/Posts.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
     title: String,
     description: String,
+    trail: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Trail'
+    }
 }, {timestamps: true});
 
 module.exports = mongoose.model('Post', PostSchema);
--- models/index.js
+++ models/index.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
-
-// may not need mongoose connection here?
-// const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/express-cities';
-
-// mongoose.connect(DB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => console.log('MongoDB connnected successfully...'))
-//   .catch((err) => console.log(err));
+// const seed = require('../seed');
 
 module.exports = {
     Trail: require('./Trails'),
