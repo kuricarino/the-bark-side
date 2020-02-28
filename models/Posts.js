f08a10cef1404b83c7eb0ea4ffe0d2457b740362
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-02-28 15:35:02 -0800

    In the process of writing and testing routes

--- models/Posts.js
+++ models/Posts.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
     title: String,
     description: String,
-    trail: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Trail'
-    }
 }, {timestamps: true});
 
 module.exports = mongoose.model('Post', PostSchema);
