47ef1824b93f826f2ce7302f27a30977543aaf6d
Uyen <ulam10@gmail.com>
2020-03-01 01:09:38 -0800

    get, show, create routes+ models+ controllers set up and tested successfully

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Require expreess and body-parser
+// Require express and body-parser
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -24,107 +24,42 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Require DB
 const db = require('./models');
-const seed = require('./seed');
-// const routes = require('./routes');
+const routes = require('./routes');
 // const views = require('./views');
 
-/***********************************
-* ROUTES *
-************************************/
+/************* ROUTES *************/
 
 // Serve public assets: `/images`, `/scripts`, `/styles`
 // app.use(express.static(__dirname/ + '/public'));
 
-// ** VIEWS ENDPOINTS ** //
+// **** VIEWS ENDPOINTS **** 
 // app.use('/', routes.views);
 
-// app.use('/api', (req,res) => {
+// **** API ENDPOINTS **** 
+app.use('/api', routes.api);
+
+// (req,res) => {
 //     res.send('<h1>Trails</h1>');
 // });
 
+// *** TRAILS *** //
 
-// * TRAILS * //
-// GET: Trails -- tested
-// app.get('/api/trails', (req, res) => {
-//     db.Trail.find({}, (err, allTrails) => {
-//         if (err) res.status(400).json({status: 400, error: `Something went wrong.`});
-//         return res.json(allTrails);
-//     });
-// });
+// UPDATE: Post  - tested 
+// app.put('/api/trails/:trailId/posts/:postId', routes.api );
 
-// * POSTS * //
-// GET: Posts -- tested
-app.get('/api/trails/:trailId/posts', (req, res) => {
-    db.Post.find({}, (err, allPosts) => {
-        if (err) res.status(400).json({status: 400, error: `Something went wrong.`});
-        return res.json(allPosts);
-    });
-});
+// DELETE: Post  
+// app.delete('/api/trails/:trailId', (req, res) => {
+//     // db.Trail.deleteOne({nickname: req.params.trailId}, (err, delPost) => {
 
-
-// SHOW: Post -- tested
-// app.get('/api/posts/:postId', (req, res) => {
-//     db.Post.findById(req.params.postId, (err, showPost) => {
-//         if (err) res.status(400).json({status: 400, error: `Something went wrong.`});
-//         return res.json(showPost)
-//     })
-// });
-
-
-// CREATE: New Post
-app.post('/api/trails/:trailId/post', (req, res) => {
-//     db.Post.create(req.body, (err, newPost) => {
-//         if (err) res.status(400).json({status: 400, error: `Something went wrong.`});
-        
-//         db.Trail.findById(req.params.trailId);
-//         const trail = req.params.trailId;
-//         trail.posts.push(newPost);
-//         //trail.posts.push(newPost);
-//         newPost.save();
-//     })
-// });
-
-
-db.Trail.deleteMany({}, (err, delTrails) => {
-    if (err) console.log('can"t delete trails', err);
-    // console.log('deleted all trails', delTrails);
-  
-    db.Trail.create({},(err, newTrails) => {
-      if (err) console.log(`can't create new trails`);
-      // console.log(newTrails);
-  
-        db.Post.create({}, (err, savedPost) => {
-          if (err) console.log(err);
-          newTrails.posts.push(savedPost);
-          newTrails.save();
-        console.log(newTrails);
-        })
-      })
-    })
-});
-
-
-// UPDATE: Post
-// app.post('/api/trails/:trailId/posts/:postId', (req, res) => {
-//     db.Post.findByIdAndUpdate(req.params.postId, req.body, {new: true}, (err, updatedPost) => {
-//         if (err) res.status(400).json({status: 400, error: `Something went wrong`});
-
-//         res.json(updatedPost);
-//     });
-// });
-
-// DELETE: Post
-// app.delete('/api/posts/:postId', (req, res) => {
-//     db.Post.
+//     // })
 // })
-
 
 // ** ERROR ENDPOINTS ** //
 
 // Error for API
-// app.use(‘/api/*‘, (req, res) => {
-//   res.send(‘<h2>Error 404: Not Found</h2>’);
-// });
+app.use('/api/*', (req, res) => {
+  res.send('<h2>Error 404: Not Found</h2>');
+});
 
 // Error for HTML 
 // app.use(‘*’, (req, res) => {
