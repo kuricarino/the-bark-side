bf9750869c843b8d8e54ff8f08f5a1d10c67c9f7
Uyen <ulam10@gmail.com>
2020-03-03 10:48:41 -0800

    updated variable names for consistency

--- public/scripts/createPost.js
+++ public/scripts/createPost.js
@@ -1,16 +1,18 @@
 // ============= CREATE POST FUNCTION ON MODAL =============//
 
-document.querySelector('#submit-post').addEventListener('click', createPost);
-const postsContainer = document.querySelector('.le-scroll-box');
+document.querySelector('ff-form').addEventListener('submit', createPost);
+const postsContainer = document.querySelector('.ff-scroll-box');
+console.log(postsContainer);
 
-function createPost(event) {
-  const title = document.querySelector('#le-post-title').value;
-  const body = document.querySelector('#le-message-text').value;
+function createPost(e) {
+  console.log('create post');
+  const title = document.querySelector('#ff-post-title').value;
+  const body = document.querySelector('#ff-message-text').value;
   // console.log(document.querySelector('#post-title').value);
   // console.log(document.querySelector('#message-text').value);
   const newPost = {title, body};
 
-  fetch('http;//localhost:5000', {
+  fetch('/api/trails/ff/posts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -19,20 +21,20 @@ function createPost(event) {
       })
       .then(stream => stream.json())
       .then(res => render(res))
+      .catch(err => console.log(err))
 };
 
-
-function render(posts) {
-  postsContainer.insertAdjacentHTML('beforeend', posts)
+function render(post) {
+  postsContainer.insertAdjacentHTML('beforeend', getPostTemplate(post))
 };
 
 function getPostTemplate(post) {
-  return 
-  `
-  <div class="post">
-    <p>${post.title}</p>
-    <p>${post.body}</p>
-  `
+  return `
+  <div class="ff-scroll-box">
+    <p>Title:${post.title}</p>
+    <p>Post:${post.body}</p>
+  </div>
+  `;
 }
 
 // ============= END OF CREATE POST ============= //
