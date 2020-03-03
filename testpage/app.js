8fba283592bb2d23ed09e54eb6a296cea41f0032
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-03-02 19:25:04 -0800

    Edited app.js under testpage branch and folder to have event listener on modal button and append object to posts-container

--- testpage/app.js
+++ testpage/app.js
@@ -17,6 +17,47 @@ $("#pop").on("click", function() {
 
 
 
+// ============= CREATE POST: Mon, March 2 =============//
+
+document.querySelector('#submit-post').addEventListener('click', createPost);
+const postsContainer = document.querySelector('.post-container');
+
+function createPost(event) {
+  // console.log(`does this work?`)
+  const title = document.querySelector('#post-title').value;
+  const body = document.querySelector('#message-text').value;
+  console.log(document.querySelector('#post-title').value);
+  console.log(document.querySelector('#message-text').value);
+  const newPost = {title, body};
+
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPost)
+      })
+      .then(stream => stream.json())
+      .then(res => render(res))
+};
+
+
+function render(posts) {
+  postsContainer.insertAdjacentHTML('beforeend', posts)
+};
+
+function getPostTemplate(post) {
+  return 
+  `
+  <div class="post">
+    <p>${post.title}</p>
+    <p>${post.body}</p>
+  `
+}
+
+// ============= END OF CREATE POST ============= //
+
+
 
 
 
