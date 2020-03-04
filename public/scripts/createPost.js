dc39b5e32402d4625813c39a980b6337fb7e36c6
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-03-03 19:56:42 -0800

    Work on frontend and getting posts to populate on page load

--- public/scripts/createPost.js
+++ public/scripts/createPost.js
@@ -1,5 +1,4 @@
 // ============= CREATE POST FUNCTION ON MODAL =============//
-// render(posts);
 
 document.querySelector('#submit-post').addEventListener('click', createPost);
 const postsContainer = document.querySelector('.ff-scroll-box');
@@ -41,3 +40,58 @@ function getPostTemplate(post) {
 }
 
 // ============= END OF CREATE POST ============= //
+
+
+// ============= GET ALL POSTS ON PAGE LOAD =============//
+
+// function getAllPosts() {
+//   //const allPosts = {posts.posts.title, description};
+//   console.log(allPosts);
+//   fetch('/api/trails/ff/posts', {
+//         method: 'GET',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(allPosts)
+//       })
+//       .then(stream => stream.json())
+//       .then(res => renderAllPosts(res))
+//       .catch(err => console.log(err))
+// };
+
+// function renderAllPosts(posts) {
+//   postsContainer.insertAdjacentHTML('beforeend', getAllPosts(posts.posts))
+// };
+
+// function allPosts(post) {
+//     return`
+//     <div class="ff-scroll-box">
+//     <p>Title: ${post.title}</p>
+//     <p>Comment:${post.description}</p>
+//     </div>
+//     `;
+// }
+
+function onPageLoad() {
+  // getAllPosts();
+  // renderAllPosts();
+  // allPosts();
+  //createPost();
+  indexPost();
+  render();
+  getPostTemplate();
+}
+
+function indexPost() {
+  // e.preventDefault(); 
+  fetch('/api/trails/ff/posts', {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'},
+  })
+  .then(response => response.json)   
+  .then(json => render(json)) 
+  .catch(err => console.log(err))
+}
+
+// ============= END OF GET ALL POSTS ON PAGE LOAD ============= //
+
