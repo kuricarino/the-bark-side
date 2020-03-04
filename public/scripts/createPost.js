535920451fcb1966842787fc9076c74d681d1eb1
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-03-04 10:45:06 -0800

    createPost can get index and create new posts

--- public/scripts/createPost.js
+++ public/scripts/createPost.js
@@ -1,21 +1,21 @@
-// ============= PRE RENDER ALL POSTS =============//
-
-//api call
-  //GET all posts
-  //render html for each POST
-
-// ============= CREATE POST FUNCTION ON MODAL =============//
+// ============= CREATE ONE POST FUNCTION ON MODAL =============//
 
 document.querySelector('#submit-post').addEventListener('click', createPost);
 const postsContainer = document.querySelector('.ff-scroll-box');
 console.log(postsContainer);
 
+function allPosts() {
+  fetch('/api/trails/ff/posts')
+  .then(stream => stream.json())
+  .then(res => renderAll(res))
+}
+
+allPosts();
 
 function createPost(event) {
   console.log('create post');
   const title = document.querySelector('#ff-post-title').value;
   const description = document.querySelector('#ff-message-text').value;
-  // console.log(description);
   const newPost = {title, description};
   console.log(newPost)
 
@@ -26,119 +26,43 @@ function createPost(event) {
         },
         body: JSON.stringify(newPost)
       })
-      // .then(stream => stream.json())
+      .then(stream => stream.json())
       .then(res => render(newPost))
       // .then(res => render(res))
       // .then(res => res.text())
-      // .then(text => console.log(text))
       .catch(err => console.log(err))
 }
 
-function render(posts) {
-  console.log(posts);
-  postsContainer.insertAdjacentHTML('beforeend', getPostTemplate(posts))
-
+function renderAll(posts) {
+  posts.forEach(post => {
+    render(post)
+  })
 }
 
-
-// commented out code below and parts above used to work but broke after pull request from submaster
-// function render(posts) {
-//   console.log(posts)
-//   for (let i = posts.posts.length - 1; i > 0; i--) {
-//     postsContainer.insertAdjacentHTML('beforeend', getPostTemplate(posts.posts[i]))
-//   }
-// };
-
-// function render(post) {
-//       postsContainer.insertAdjacentHTML('beforeend', getPostTemplate(post))
-//   };
+function render(post) {
+  console.log(post);
+  postsContainer.insertAdjacentHTML('afterbegin', getPostTemplate(post))
+}
 
 function getPostTemplate(post) {
     return`
     <div class="ff-scroll-box">
     <p>Title: ${post.title}</p>
-    <p>Comment:${post.description}</p>
+    <p>Comment: ${post.description}</p>
     <button>Update</button>
     <button>Delete</button>
     </div>
     `;
 }
 
-// UPDATE
-// document.querySelector('#submit-post').addEventListener('click', createPost);
-
-
-$("#pop").on("click", function(e) {
-  e.preventDefault();
-  $('#le-npsimg').modal('toggle');
-});
-// ============= END OF CREATE POST ============= //
+// ============= END OF CREATE NEW POST ============= //
 
 
 
 
 
-
-// ============= PRE RENDER ALL POSTS =============//
-
-//api call
-  //GET all posts
-  //render html for each POST
-
-// ============= CREATE POST FUNCTION ON MODAL =============//
-
-// document.querySelector('#submit-post').addEventListener('click', createPost);
-// const postsContainer = document.querySelector('.ff-scroll-box');
-// console.log(postsContainer);
-
-
-// function createPost(event) {
-//   console.log('create post');
-//   const title = document.querySelector('#ff-post-title').value;
-//   const description = document.querySelector('#ff-message-text').value;
-//   const newPost = {title, description};
-
-//   fetch('/api/trails/ff/posts', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(newPost)
-//       })
-//       .then(stream => stream.json())
-//       .then(res => render(res))
-//       .catch(err => console.log(err))
-// };
-
-// function render(posts) {
-//   console.log(posts);
-//   for (let i = posts.posts.length - 1; i > 0; i--) {
-//     postsContainer.insertAdjacentHTML('beforeend', getPostTemplate(posts.posts[i]))
-//   }
-// };
-
-// function render(post) {
-//       postsContainer.insertAdjacentHTML('beforeend', getPostTemplate(post))
-//   };
-
-// function getPostTemplate(post) {
-//     return`
-//     <div class="ff-scroll-box">
-//     <p>Title: ${post.title}</p>
-//     <p>Comment:${post.description}</p>
-//     <button>Update</button>
-//     <button>Delete</button>
-//     </div>
-//     `;
-// }
-
-// // UPDATE
-// document.querySelector('#submit-post').addEventListener('click', createPost);
-
-
+// FOR LEASH MAP
 // $("#pop").on("click", function(e) {
 //   e.preventDefault();
 //   $('#le-npsimg').modal('toggle');
 // });
-// // ============= END OF CREATE POST ============= //
-
