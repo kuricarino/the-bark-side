25f2b0f2a40aa2c7255ef838ebfefeeff5e9f54d
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-03-03 16:02:05 -0800

    creatPost WIP

--- public/scripts/createPost.js
+++ public/scripts/createPost.js
@@ -1,16 +1,18 @@
 // ============= CREATE POST FUNCTION ON MODAL =============//
+// loadPosts();
 
-document.querySelector('ff-form').addEventListener('submit', createPost);
+document.querySelector('#submit-post').addEventListener('click', createPost);
+// console.log(document.querySelector('#submit-post'));
 const postsContainer = document.querySelector('.ff-scroll-box');
 console.log(postsContainer);
 
-function createPost(e) {
+function createPost(event) {
   console.log('create post');
   const title = document.querySelector('#ff-post-title').value;
-  const body = document.querySelector('#ff-message-text').value;
+  const description = document.querySelector('#ff-message-text').value;
   // console.log(document.querySelector('#post-title').value);
   // console.log(document.querySelector('#message-text').value);
-  const newPost = {title, body};
+  const newPost = {title, description};
 
   fetch('/api/trails/ff/posts', {
         method: 'POST',
@@ -29,12 +31,38 @@ function render(post) {
 };
 
 function getPostTemplate(post) {
-  return `
+  // console.log(post);
+  // console.log(post.posts);
+  for (let i = 0; i < post.posts.length; i++) {
+  ` 
   <div class="ff-scroll-box">
-    <p>Title:${post.title}</p>
-    <p>Post:${post.body}</p>
+    <p>Title:${post.posts[i].title}</p>
+    <p>Post:${post.posts[i].description}</p>
   </div>
-  `;
+  `
+  }
+  // return ` 
+  // <div class="ff-scroll-box">
+  //   <p>Title:${post.posts[0].title}</p>
+  //   <p>Post:${post.posts[0].description}</p>
+  // </div>
+  // `;
 }
+
+
+
+// Create a function for when first visit page and loads every single post
+
+// function loadPosts() {
+//   post.posts.populate()
+//   .exec((err, post) => {
+//     if (err) return res.status(400).json({msg: 'Book ID does not exist'});
+//     res.json(post)
+//   })
+// }
+
+
+// Another function for when they create a new post that just returns new post  
+
 
 // ============= END OF CREATE POST ============= //
